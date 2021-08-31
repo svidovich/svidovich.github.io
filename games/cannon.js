57d13a605a3cf595806fca1316ae8286dbcb374e
mydriasis <samuel.vidovich@gmail.com>
2021-08-30 20:14:24 -0400

    Re-implement the firing mechanism so that it depends on vector decomposition

--- games/cannon.js
+++ games/cannon.js
@@ -4,8 +4,8 @@ let canvas = document.getElementById("mainCanvas");
 let canvasHeight = canvas.height;
 let canvasWidth = canvas.width;
 let canvasContext = canvas.getContext("2d");
-// So that we know the limits of our canvas
-let boundingClientRectangle = canvas.getBoundingClientRect();
+// Will projectiles bounce?
+const projectilesWillBounce = false;
 // General Constants for Gameplay
 const cursorSize = 15;
 // Keep this even because I don't feel like programming around
@@ -26,8 +26,8 @@ class MouseFollowingInput {
   }
 
   register() {
-    // Add listeners for the mouse
-    console.log("registering...");
+    // Adding listeners for the mouse
+
     // Binding
     // Here, when we add the event listener, and when updateMouseState is called
     // as a callback, _inside of updateMouseState_ 'this' is NOT the class. It's
@@ -84,40 +84,18 @@ class CannonData {
 }
 
 class Projectile {
-  constructor(x, y, destX, destY, speed) {
+  constructor(x, y, angle, speed) {
     this.x = x;
     this.y = y;
+    this.angle = angle;
     this.speed = speed;
     this.hasBounced = false;
-
-    // I hate this approach. Too many corner cases.
-    // Next time we'll figure out what was wrong with the angles
-    console.log(Math.abs(destX - x));
-    if (Math.abs(destX - x) < 20) {
-      this.slope = 10;
-    } else {
-      this.slope = (destY - y) / (destX - x);
-    }
-    // If we're in quadrant III or IV, we will need
-    // to augment the calculation so that as we update
-    // x, we're subtracting instead of adding. To do so,
-    // detect our quadrant, then revert the speed
-    if (destX < x) {
-      this.speed = -this.speed;
-    }
+    this.queueDeletion = false;
 
     this.setInMotion();
   }
 
   setInMotion() {
-    // This is what I want to do but it didn't work well the first time
-    // so i went with a different approach. Fml, I hate the new approach
-    // I know the angle at which the barrel of the gun firing
-    // this projectile is pointed, but I need a slope to give my
-    // projectile direction. Slope, being 'rise-over-run', is then
-    // equivalent to the tangent of my angle, y/x or opposite-over-
-    // adjacent.
-    let ySign = 1;
     // I'm lifting some of this logic from what I've learned in the
     // bubbles game I made.
     setInterval(() => {
@@ -125,37 +103,42 @@ class Projectile {
       // cause us to 'bounce off' the side, and start moving in a different direction.
       // We only get to bounce off once. After that, we need to destroy ourselves.
 
-      // if (this.x >= canvasWidth || this.x <= 0) {
-      //   if (this.hasBounced === false) {
-      //     this.slope = -this.slope;
-      //     this.hasBounced === true;
-      //   } else {
-      //     console.log("I should garbage collect myself now.");
-      //   }
-      // }
+      if (this.x >= canvasWidth || this.x <= 0) {
+        if (projectilesWillBounce) {
+          if (this.hasBounced === false) {
+            this.speed = -this.speed;
+            this.hasBounced = true;
+          } else {
+            this.queueDeletion = true;
+          }
+        } else {
+          this.queueDeletion = true;
+        }
+      }
 
       // If we've hit the top or bottom, we should reverse the sign of our y value. This
       // will cause us to start moving in the opposite direction with respect to the y
       // axis, which is just a bounce off the top or bottom.
       // We only get to bounce off once. After that, we need to destroy ourselves.
 
-      // if (this.y >= canvasHeight || this.y <= 0) {
-      //   if (this.hasBounced === false) {
-      //     ySign = -ySign;
-      //     this.hasBounced === true;
-      //   } else {
-      //     console.log("I should garbage collect myself now.");
-      //   }
-      // }
+      if (this.y >= canvasHeight || this.y <= 0) {
+        if (projectilesWillBounce) {
+          if (this.hasBounced === false) {
+            this.speed = -this.speed;
+            this.hasBounced = true;
+          } else {
+            this.queueDeletion = true;
+          }
+        } else {
+          this.queueDeletion = true;
+        }
+      }
 
-      // Finally, given our computed slope and y sign, whatever they are, update our
-      // location.
-      let previousX = this.x;
-      let previousY = this.y;
-      this.x += this.speed;
-      this.y += this.slope * (this.x - previousX); // * this.x + 1 * ySign;
-      // this.x += this.slope;
-      // this.y += 1 * ySign;
+      // The next position is based on a simple vector decomposition.
+      // Here, the magnitude is the speed, and then we decompose the
+      // vector angle into its consituent parts.
+      this.x += this.speed * Math.cos((this.angle * Math.PI) / 180);
+      this.y += this.speed * Math.sin((this.angle * Math.PI) / 180);
     }, 50);
   }
 }
@@ -238,16 +221,20 @@ const projectileSpeed = 50;
 const projectileSize = 5;
 const onScreenProjectiles = new Array();
 const fireCannon = (cannonData, mouseClickEvent) => {
-  let x = cannonData.coordinates.x;
-  let y = cannonData.coordinates.y;
-  // We need to compute the difference between the inside of the canvas
-  // and the edge of the screen. The mouse event is nice, but it doesn't
-  // give us coordinates inside of the canvas.
-  let destX = mouseClickEvent.clientX - boundingClientRectangle.left;
-  let destY = mouseClickEvent.clientY - boundingClientRectangle.top;
-  let firedProjectile = new Projectile(x, y, destX, destY, projectileSpeed);
+  const x = cannonData.coordinates.x;
+  const y = cannonData.coordinates.y;
+  const angle = cannonData.angle;
+  let firedProjectile = new Projectile(x, y, angle, projectileSpeed);
   onScreenProjectiles.push(firedProjectile);
-  console.log(onScreenProjectiles);
+};
+
+const garbageCollectProjectiles = (projectilesArray) => {
+  projectilesArray.forEach((projectile) => {
+    if (projectile.queueDeletion === true) {
+      const projectileIndex = projectilesArray.indexOf(projectile);
+      projectilesArray.splice(projectileIndex, 1);
+    }
+  });
 };
 
 canvas.addEventListener("click", fireCannon.bind(null, cannon1));
@@ -263,6 +250,7 @@ const update = () => {
   onScreenProjectiles.forEach((element) => {
     drawCircle(canvasContext, element.x, element.y, projectileSize);
   });
+  garbageCollectProjectiles(onScreenProjectiles);
 };
 
 (() => {
