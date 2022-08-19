5e1c061cb5e4b4ef8d846e54a8d82fe0b91d6e96
sam@paimon <samuel.vidovich@gmail.com>
2022-08-18 23:27:32 -0400

    Delete some dead code. Start new header.

--- index.js
+++ index.js
@@ -1,81 +1,90 @@
-// Initialize the state of the description frames to empty, because they are
-// empty on pageload.
-localStorage.setItem('workExperienceDescriptionState', "empty");
-localStorage.setItem('programmingExperienceDescriptionState', "empty");
-// Get the description divs so that we can fill them with content later.
-let workExperienceDescriptionDiv = document.getElementById('work-experience-description');
-let programmingExperienceDescriptionDiv = document.getElementById('programming-experience-description');
+const canvas = document.getElementById("headerCanvas");
+const canvasHeight = canvas.height;
+const canvasWidth = canvas.width;
+const canvasContext = canvas.getContext("2d");
 
-// A little bait-and-switch action. Set state based on the description ID, so I can switch back and
-// forth between multiple clickables on the page easily.
-let SetDescription = (descriptionElement, stateString, Description) => {
-    if (localStorage.getItem(stateString) === "empty" || localStorage.getItem(stateString) !== Description.id) {
-        console.log(Description.id)
-        localStorage.setItem(stateString, Description.id);
-        descriptionElement.innerHTML = Description.data;
-    } else {
-        localStorage.setItem(stateString, "empty");
-        descriptionElement.innerHTML = "";
-    }
-}
+const NAME_COLOR_CHANGE_SPEED_MS = 100;
+const NAME_COLOR_CHANGE_QUANTITY = 5;
 
-// A function that allows me to pull HTML from a given uri.
-// Returns a promise that I can then resolve to the data I need.
-const GetHTMLFromURI = async (uri) => {
-    return new Promise((resolve, reject) => {
-        fetch(uri).then(response => { return resolve(response.text()) });
-    })
+const generateRandomNumber = (min, max) => {
+  return Math.floor(Math.random() * (Math.floor(max) - Math.ceil(min)) + Math.ceil(min));
 };
 
+let nameFontState = {
+  r: 0,
+  g: 0,
+  b: 0,
+};
 
-let fsDescription = new Object();
-fsDescription.id = 'finite-state-description';
-GetHTMLFromURI('./assets/elements/finite_state_description.html').then(data => { fsDescription.data = data });
-let finiteStateHoverable = document.getElementById('fs-hoverable');
-finiteStateHoverable.addEventListener('click', SetDescription.bind(this, workExperienceDescriptionDiv, 'workExperienceDescriptionState', fsDescription));
+const advanceNameFontState = () => {
+  nameFontState.r = (nameFontState.r + NAME_COLOR_CHANGE_QUANTITY) % 255;
+  nameFontState.g = (nameFontState.g + NAME_COLOR_CHANGE_QUANTITY) % 255;
+  nameFontState.b = (nameFontState.b + NAME_COLOR_CHANGE_QUANTITY) % 255;
+};
 
+const randomizeNameFontState = () => {
+  nameFontState.r = generateRandomNumber(1, 255);
+  nameFontState.g = generateRandomNumber(1, 255);
+  nameFontState.b = generateRandomNumber(1, 255);
+};
 
-let awsDescription = new Object();
-awsDescription.id = 'aws-description';
-GetHTMLFromURI('./assets/elements/aws_description.html').then(data => { awsDescription.data = data });
-let awsHoverable = document.getElementById('aws-hoverable');
-awsHoverable.addEventListener('click', SetDescription.bind(this, programmingExperienceDescriptionDiv, 'programmingExperienceDescriptionState', awsDescription));
+const drawName = () => {
+  //   canvasContext.strokeRect(0, 0, canvasWidth, canvasHeight);
+  let oldFont = canvasContext.font;
+  let oldFillStyle = canvasContext.fillStyle;
+  canvasContext.font = "25px Courier";
+  canvasContext.fillStyle = `rgb(${nameFontState.r}, ${nameFontState.g}, ${nameFontState.b})`;
+  canvasContext.fillText("Samuel Vidovich", canvasWidth / 3, canvasHeight / 2);
+  canvasContext.font = oldFont;
+  canvasContext.fillStyle = oldFillStyle;
+};
 
+const bubblesArray = new Array();
 
-pythonDescription = new Object();
-pythonDescription.id = 'python-description';
-GetHTMLFromURI('./assets/elements/python_description.html').then(data => { pythonDescription.data = data });
-let pythonHoverable = document.getElementById('python-hoverable');
-pythonHoverable.addEventListener('click', SetDescription.bind(this, programmingExperienceDescriptionDiv, 'programmingExperienceDescriptionState', pythonDescription));
+class Bubble {
+  constructor(x, y, r, speed) {
+    this.x = x;
+    this.y = y;
+    this.r = r;
+    this.speed = speed;
+  }
+  get coordinates() {
+    return {
+      x: this.x,
+      y: this.y,
+    };
+  }
+  moveBy(dx, dy) {
+    this.x += dx;
+    this.y += dy;
+  }
+}
 
+const randomBubbleFactory = () => {};
 
-sparkDescription = new Object();
-sparkDescription.id = 'spark-description';
-GetHTMLFromURI('./assets/elements/spark_description.html').then(data => { sparkDescription.data = data });
-let sparkHoverable = document.getElementById('spark-hoverable');
-sparkHoverable.addEventListener('click', SetDescription.bind(this, programmingExperienceDescriptionDiv, 'programmingExperienceDescriptionState', sparkDescription));
+const update = () => {
+  drawName();
+};
 
+(() => {
+  let animationFrameRequestToken;
 
+  // Set state of name colours here to guarantee no race
+  // condition
+  randomizeNameFontState();
+  // Change the colour of the name rather quickly,
+  window.setInterval(advanceNameFontState, NAME_COLOR_CHANGE_SPEED_MS);
+  // And reset it to something random rather less quickly.
+  window.setInterval(randomizeNameFontState, NAME_COLOR_CHANGE_SPEED_MS * 100);
 
-// TODOS!
-underConstructionDescription = new Object();
-underConstructionDescription.id = 'under-construction';
-GetHTMLFromURI('./assets/elements/under_construction.html').then(data => { underConstructionDescription.data = data });
+  main = (hiResTimeStamp) => {
+    try {
+      animationFrameRequestToken = window.requestAnimationFrame(main);
 
-pgDescription = new Object();
-pgDescription.id = 'pg-description';
-GetHTMLFromURI('./assets/elements/postgres_description.html').then(data => {pgDescription.data = data});
-let pgHoverable = document.getElementById('pg-hoverable');
-pgHoverable.addEventListener('click', SetDescription.bind(this, programmingExperienceDescriptionDiv, 'programmingExperienceDescriptionState', pgDescription));
-
-dockerDescription = new Object();
-dockerDescription.id = 'docker-description'
-GetHTMLFromURI('./assets/elements/docker_description.html').then(data => {dockerDescription.data = data});
-let dockerHoverable = document.getElementById('docker-hoverable');
-dockerHoverable.addEventListener('click', SetDescription.bind(this, programmingExperienceDescriptionDiv, 'programmingExperienceDescriptionState', dockerDescription));
-
-linuxDescription = new Object();
-linuxDescription.id = 'linux-description'
-GetHTMLFromURI('./assets/elements/linux_description.html').then(data => {linuxDescription.data = data});
-let linuxHoverable = document.getElementById('linux-hoverable');
-linuxHoverable.addEventListener('click', SetDescription.bind(this, programmingExperienceDescriptionDiv, 'programmingExperienceDescriptionState', linuxDescription));
+      update();
+    } catch (error) {
+      console.error(error);
+    }
+  };
+  main();
+})();
