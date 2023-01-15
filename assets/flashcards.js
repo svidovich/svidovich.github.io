a3a2edcc3cd35a4050c437f87b1f33451b0beec2
sam@paimon <samuel.vidovich@gmail.com>
2023-01-15 02:25:07 -0500

    ship it

--- assets/flashcards.js
+++ assets/flashcards.js
@@ -1,11 +1,37 @@
-import { latinToJugoslavCyrillic } from "./flashcarddata.js";
+import { UNIT_4_VOCAB } from "./flashcarddata.js";
 
+// Stolen UUID generator.
 const UUIDGeneratorBrowser = () =>
   ([1e7] + -1e3 + -4e3 + -8e3 + -1e11).replace(/[018]/g, (c) =>
     (c ^ (crypto.getRandomValues(new Uint8Array(1))[0] & (15 >> (c / 4)))).toString(16)
   );
 
-const addcard = () => {
+// Stolen array shuffler.
+// https://stackoverflow.com/questions/2450954/how-to-randomize-shuffle-a-javascript-array
+function shuffle(array) {
+  let currentIndex = array.length,
+    randomIndex;
+
+  // While there remain elements to shuffle.
+  while (currentIndex != 0) {
+    // Pick a remaining element.
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex--;
+
+    // And swap it with the current element.
+    [array[currentIndex], array[randomIndex]] = [array[randomIndex], array[currentIndex]];
+  }
+
+  return array;
+}
+
+const randomInt = (min, max) => {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1) + min);
+};
+
+const addCard = (front, back) => {
   const flashCardContainer = document.getElementById("flashcardcontainer");
   const id = UUIDGeneratorBrowser();
   const scene = document.createElement("div");
@@ -19,7 +45,7 @@ const addcard = () => {
     cardFace.classList.toggle("card__face");
     cardFace.classList.toggle(`card__face--${faceType}`);
     cardFace.setAttribute(`face-${faceType}-id`, id);
-    const textContent = document.createTextNode("Argh me bones");
+    const textContent = document.createTextNode(faceType === "front" ? front : back);
     cardFace.appendChild(textContent);
     card.appendChild(cardFace);
   });
@@ -30,13 +56,36 @@ const addcard = () => {
 
   card.addEventListener("click", () => {
     card.classList.toggle("is-flipped");
-    console.log(`${id}, ${card.classList}`);
   });
-  console.log(`Card with id ${id} added.`);
 };
 
-let counter = 0;
 (() => {
-  addcard();
-  addcard();
+  let vocabCopy = [...UNIT_4_VOCAB];
+  shuffle(vocabCopy);
+  vocabCopy.forEach((vocabularyObject) => {
+    // Let's flip some coins, shall we?
+    let front;
+    let rear;
+    if (randomInt(0, 100) > 50) {
+      // In this case, we've got Yugo on the front.
+      // Cyrillic, or latin?
+      if (randomInt(0, 100) > 50) {
+        front = vocabularyObject.cyrillic;
+      } else {
+        front = vocabularyObject.latin;
+      }
+      rear = vocabularyObject.english;
+    } else {
+      // Now, the front is English.
+      front = vocabularyObject.english;
+      // In this case, we've got Yugo on the rear.
+      // Cyrillic, or latin?
+      if (randomInt(0, 100) > 50) {
+        rear = vocabularyObject.cyrillic;
+      } else {
+        rear = vocabularyObject.latin;
+      }
+    }
+    addCard(front, rear);
+  });
 })();
