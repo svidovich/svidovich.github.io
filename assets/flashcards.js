3335b7653dafbf545a39495e65b4fd52ae8cef2e
sam@paimon <samuel.vidovich@gmail.com>
2023-01-18 23:24:24 -0500

    Ready up for mobile. New platform detecting script.

--- assets/flashcards.js
+++ assets/flashcards.js
@@ -1,6 +1,37 @@
 import { FORMAT_FLASHCARDS, FORMAT_QUIZ } from "./flashcarddata.js";
 import { practiceMap } from "./flashcarddata.js";
 
+const PLATFORMS = Object.freeze({
+  MACOS: "macos",
+  IOS: "ios",
+  WINDOWS: "windows",
+  ANDROID: "android",
+  LINUX: "linux",
+  UNKNOWN: "unknown",
+});
+// Stolen OS Detector. See:
+// https://stackoverflow.com/questions/38241480/detect-macos-ios-windows-android-and-linux-os-with-js
+const getOS = () => {
+  const userAgent = window.navigator.userAgent;
+  const platform = window.navigator?.userAgentData?.platform || window.navigator.platform;
+  const macosPlatforms = ["Macintosh", "MacIntel", "MacPPC", "Mac68K"];
+  const windowsPlatforms = ["Win32", "Win64", "Windows", "WinCE"];
+  const iosPlatforms = ["iPhone", "iPad", "iPod"];
+
+  if (macosPlatforms.indexOf(platform) !== -1) {
+    return PLATFORMS.MACOS;
+  } else if (iosPlatforms.indexOf(platform) !== -1) {
+    return PLATFORMS.IOS;
+  } else if (windowsPlatforms.indexOf(platform) !== -1) {
+    return PLATFORMS.WINDOWS;
+  } else if (/Android/.test(userAgent)) {
+    return PLATFORMS.ANDROID;
+  } else if (/Linux/.test(platform)) {
+    return PLATFORMS.LINUX;
+  }
+  return PLATFORMS.UNKNOWN;
+};
+
 // Global for storing practice document state
 const practiceState = new Array();
 
@@ -671,4 +702,8 @@ const main = () => {
 
 (() => {
   main();
+  const debugMessage = document.getElementById("debugmessage");
+  const debugText = document.createTextNode(`${navigator.userAgent} ${navigator.platform}`);
+  debugMessage.appendChild(debugText);
+  debugMessage.hidden = false;
 })();
