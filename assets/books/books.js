ae6fbb6eaef1f0153ae498019c2621e528f3d89e
sam@paimon <samuel.vidovich@gmail.com>
2023-07-25 23:41:28 -0400

    Accidentally reactive canvas

--- assets/books/books.js
+++ assets/books/books.js
@@ -6,7 +6,7 @@ const parentFrameHeight = canvas.parentElement.clientHeight;
 canvas.width = (95 / 100) * parentFrameWidth;
 canvas.height = (95 / 100) * parentFrameHeight;
 const canvasContext = canvas.getContext("2d");
-const FONT_SIZE_PX = 10;
+const FONT_SIZE_PX = 18;
 
 for (const [index, sentence] of Object.entries(CrvenkapaBook.sentences)) {
   console.log(sentence.jugoslavian);
@@ -14,13 +14,17 @@ for (const [index, sentence] of Object.entries(CrvenkapaBook.sentences)) {
 window.devicePixelRatio = 2;
 canvasContext.scale(window.devicePixelRatio, window.devicePixelRatio);
 const update = () => {
+  const parentFrameWidth = canvas.parentElement.clientWidth;
+  const parentFrameHeight = canvas.parentElement.clientHeight;
+  canvas.width = (95 / 100) * parentFrameWidth;
+  canvas.height = (95 / 100) * parentFrameHeight;
   canvasContext.clearRect(0, 0, canvas.width, canvas.height);
   for (const [index, sentence] of Object.entries(CrvenkapaBook.sentences)) {
     const oldFont = canvasContext.font;
     const oldAlign = canvasContext.textAlign;
     canvasContext.textAlign = "left";
     canvasContext.font = `${FONT_SIZE_PX}px Arial`;
-    canvasContext.fillText(sentence.jugoslavian, 5, 10 + 1.5 * FONT_SIZE_PX * index);
+    canvasContext.fillText(sentence.jugoslavian, 5, FONT_SIZE_PX + 1.5 * FONT_SIZE_PX * index);
     canvasContext.font = oldFont;
     canvasContext.textAlign = oldAlign;
   }
