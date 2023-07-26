c421ac04077e3e41746d2e207c89d24360f0c39e
sam@paimon <samuel.vidovich@gmail.com>
2023-07-25 23:39:38 -0400

    Add WIP books :)

---
+++ assets/books/books.js
@@ -0,0 +1,42 @@
+import { CrvenkapaBook } from "./data/crvenkapa.js";
+
+const canvas = document.getElementById("workspacecanvas");
+const parentFrameWidth = canvas.parentElement.clientWidth;
+const parentFrameHeight = canvas.parentElement.clientHeight;
+canvas.width = (95 / 100) * parentFrameWidth;
+canvas.height = (95 / 100) * parentFrameHeight;
+const canvasContext = canvas.getContext("2d");
+const FONT_SIZE_PX = 10;
+
+for (const [index, sentence] of Object.entries(CrvenkapaBook.sentences)) {
+  console.log(sentence.jugoslavian);
+}
+window.devicePixelRatio = 2;
+canvasContext.scale(window.devicePixelRatio, window.devicePixelRatio);
+const update = () => {
+  canvasContext.clearRect(0, 0, canvas.width, canvas.height);
+  for (const [index, sentence] of Object.entries(CrvenkapaBook.sentences)) {
+    const oldFont = canvasContext.font;
+    const oldAlign = canvasContext.textAlign;
+    canvasContext.textAlign = "left";
+    canvasContext.font = `${FONT_SIZE_PX}px Arial`;
+    canvasContext.fillText(sentence.jugoslavian, 5, 10 + 1.5 * FONT_SIZE_PX * index);
+    canvasContext.font = oldFont;
+    canvasContext.textAlign = oldAlign;
+  }
+};
+
+(() => {
+  let animationFrameRequestToken;
+  const main = (hiResTimeStamp) => {
+    try {
+      animationFrameRequestToken = window.requestAnimationFrame(main);
+
+      update();
+    } catch (error) {
+      console.error(error);
+      window.webkitCancelAnimationFrame(animationFrameRequestToken);
+    }
+  };
+  main();
+})();
---
+++ assets/books/data/book.js
@@ -0,0 +1,27 @@
+export class Sentence {
+  constructor(jugoslavian, english) {
+    // Save the Jugo sentence variant and its word order
+    this.jugoslavian = jugoslavian;
+    this.jugoslavianOrder = new Object();
+    this.jugoslavian.split(" ").forEach((word, index) => {
+      this.jugoslavianOrder[index] = word;
+    });
+
+    // Save the English sentence variant and its word order
+    this.english = english;
+    this.englishOrder = new Object();
+    this.english.split(" ").forEach((word, index) => {
+      this.englishOrder[index] = word;
+    });
+  }
+}
+
+export class Book {
+  constructor(sentences) {
+    // sentences: [Sentence]
+    this.sentences = new Object();
+    sentences.forEach((sentence, index) => {
+      this.sentences[index] = sentence;
+    });
+  }
+}
