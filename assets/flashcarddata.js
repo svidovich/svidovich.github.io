cfcdc557767811a708409ed29360ba532b948056
sam@paimon <samuel.vidovich@gmail.com>
2023-09-03 11:48:47 -0400

    Start conversion

--- assets/flashcarddata.js
+++ assets/flashcarddata.js
@@ -1,182 +1,46 @@
-export const FORMAT_FLASHCARDS = "flashcards";
-export const FORMAT_QUIZ = "quiz";
-export const FORMAT_T_OR_F = "true or false";
+import { vocabularySectionFromArray, VocabularySection, VocabularyObject } from "./cards/vocabulary.js";
 
-class VocabularyObject {
-  constructor(english, latin) {
-    this.english = english;
-    this.latin = latin;
-    this.cyrillic = latinToJugoslavCyrillic(latin);
-  }
-
-  get asObject() {
-    return {
-      english: this.english,
-      latin: this.latin,
-      cyrillic: this.cyrillic,
-    };
-  }
-}
-
-class VocabularySection {
-  constructor(friendlyName, unfriendlyName, vocabularyObjects) {
-    this.friendlyName = friendlyName;
-    this.unfriendlyName = unfriendlyName;
-    this.vocabularyObjects = vocabularyObjects;
-    this.formats = [FORMAT_FLASHCARDS, FORMAT_QUIZ, FORMAT_T_OR_F];
-  }
-
-  get asObject() {
-    const vocabularyObjects = new Array();
-    this.vocabularyObjects.forEach((obj) => {
-      vocabularyObjects.push(obj.asObject);
-    });
-    return {
-      friendlyName: this.friendlyName,
-      unfriendlyName: this.unfriendlyName,
-      vocabularyObjects: vocabularyObjects,
-    };
-  }
-
-  get asJSON() {
-    return JSON.stringify(this.asObject.vocabularyObjects);
-  }
-
-  get asCSV() {
-    let textCSV = `english,latin,cyrillic\n`;
-    this.vocabularyObjects.forEach((vocabularyObject) => {
-      textCSV = textCSV.concat(`${vocabularyObject.english},${vocabularyObject.latin},${vocabularyObject.cyrillic}\n`);
-    });
-    return textCSV;
-  }
-}
-
-const JUGOSLAV_SINGLE_CHARACTER_MAP = {
-  A: "А",
-  a: "а",
-  B: "Б",
-  b: "б",
-  V: "В",
-  v: "в",
-  G: "Г",
-  g: "г",
-  D: "Д",
-  d: "д",
-  Đ: "Ђ",
-  đ: "ђ",
-  E: "Е",
-  e: "е",
-  Ž: "Ж",
-  ž: "ж",
-  Z: "З",
-  z: "з",
-  I: "И",
-  i: "и",
-  J: "Ј",
-  j: "ј",
-  K: "К",
-  k: "к",
-  L: "Л",
-  l: "л",
-  M: "М",
-  m: "м",
-  N: "Н",
-  n: "н",
-  O: "О",
-  o: "о",
-  P: "П",
-  p: "п",
-  R: "Р",
-  r: "р",
-  S: "С",
-  s: "с",
-  T: "Т",
-  t: "т",
-  Ć: "Ћ",
-  ć: "ћ",
-  U: "У",
-  u: "у",
-  F: "Ф",
-  f: "ф",
-  H: "Х",
-  h: "х",
-  C: "Ц",
-  c: "ц",
-  Č: "Ч",
-  č: "ч",
-  Š: "Ш",
-  š: "ш",
-};
-
-export const latinToJugoslavCyrillic = (inputString) => {
-  // Manually replace the  two-character substitutions
-  let incompleteReplacement = inputString
-    .replace("Nj", "Њ")
-    .replace("nj", "њ")
-    .replace("Dž", "Џ")
-    .replace("dž", "џ")
-    .replace("Lj", "Љ")
-    .replace("lj", "љ");
-  let newString = "";
-  [...incompleteReplacement].forEach((character) => {
-    if (JUGOSLAV_SINGLE_CHARACTER_MAP.hasOwnProperty(character)) {
-      newString += JUGOSLAV_SINGLE_CHARACTER_MAP[character];
-    } else {
-      newString += character;
-    }
-  });
-  return newString;
-};
-
-// For your copy-pasting pleasure
-// Đ đ
-// Ž ž
-// Ć ć
-// Č č
-// Dž dž
-// Š š
-
-export const ADJECTIVES_1 = new VocabularySection("Adjectives I", "adjectives1", [
-  new VocabularyObject("Good", "Dobar"),
-  new VocabularyObject("New", "Nov"),
-  new VocabularyObject("First", "Prv"),
-  new VocabularyObject("Last", "Poslednji"),
-  new VocabularyObject("Long", "Dug"),
-  new VocabularyObject("Own", "Svoj"),
-  new VocabularyObject("Other", "Drug"),
-  new VocabularyObject("Old", "Star"),
-  new VocabularyObject("Right", "Desni"),
-  new VocabularyObject("Big", "Velik"),
-  new VocabularyObject("High", "Visok"),
-  new VocabularyObject("Different", "Razni"),
-  new VocabularyObject("Small", "Mali"),
-  new VocabularyObject("Next", "Sljedeći"),
-  new VocabularyObject("Early", "Rano"),
-  new VocabularyObject("Young", "Mladi"),
-  new VocabularyObject("Important", "Važan"),
+export const ADJECTIVES_1 = vocabularySectionFromArray("Adjectives I", "adjectives1", [
+  { english: "Good", latin: "Dobar" },
+  { english: "New", latin: "Nov" },
+  { english: "First", latin: "Prv" },
+  { english: "Last", latin: "Poslednji" },
+  { english: "Long", latin: "Dug" },
+  { english: "Own", latin: "Svoj" },
+  { english: "Other", latin: "Drug" },
+  { english: "Old", latin: "Star" },
+  { english: "Right", latin: "Desni" },
+  { english: "Big", latin: "Velik" },
+  { english: "High", latin: "Visok" },
+  { english: "Different", latin: "Razni" },
+  { english: "Small", latin: "Mali" },
+  { english: "Next", latin: "Sljedeći" },
+  { english: "Early", latin: "Rano" },
+  { english: "Young", latin: "Mladi" },
+  { english: "Important", latin: "Važan" },
 ]);
 
-export const ADJECTIVES_2 = new VocabularySection("Adjectives II", "adjectives2", [
-  new VocabularyObject("Few", "Nekolicini"),
-  new VocabularyObject("Public", "Državni"),
-  new VocabularyObject("Bad", "Loš"),
-  new VocabularyObject("Able", "Sposoban"),
-  new VocabularyObject("Specific", "Naročit"),
-  new VocabularyObject("General", "Opšti"),
-  new VocabularyObject("Certain", "Siguran"),
-  new VocabularyObject("Free", "Slobod"),
-  new VocabularyObject("Open", "Otvoren"),
-  new VocabularyObject("Whole", "Ceo"),
-  new VocabularyObject("Short", "Kratak"),
-  new VocabularyObject("Easy", "Lak"),
-  new VocabularyObject("Strong", "Jak"),
-  new VocabularyObject("Special", "Poseban"),
-  new VocabularyObject("Clear", "Jasan"),
-  new VocabularyObject("Recent", "Nedavan"),
-  new VocabularyObject("Late", "Kasan"),
-  new VocabularyObject("Single", "Jedan"),
-  new VocabularyObject("Medical", "Medicinski"),
-  new VocabularyObject("Central", "Centralan"),
+export const ADJECTIVES_2 = vocabularySectionFromArray("Adjectives II", "adjectives2", [
+  { english: "Few", latin: "Nekolicini" },
+  { english: "Public", latin: "Državni" },
+  { english: "Bad", latin: "Loš" },
+  { english: "Able", latin: "Sposoban" },
+  { english: "Specific", latin: "Naročit" },
+  { english: "General", latin: "Opšti" },
+  { english: "Certain", latin: "Siguran" },
+  { english: "Free", latin: "Slobod" },
+  { english: "Open", latin: "Otvoren" },
+  { english: "Whole", latin: "Ceo" },
+  { english: "Short", latin: "Kratak" },
+  { english: "Easy", latin: "Lak" },
+  { english: "Strong", latin: "Jak" },
+  { english: "Special", latin: "Poseban" },
+  { english: "Clear", latin: "Jasan" },
+  { english: "Recent", latin: "Nedavan" },
+  { english: "Late", latin: "Kasan" },
+  { english: "Single", latin: "Jedan" },
+  { english: "Medical", latin: "Medicinski" },
+  { english: "Central", latin: "Centralan" },
 ]);
 
 export const ADJECTIVES_3 = new VocabularySection("Adjectives III", "adjectives3", [
