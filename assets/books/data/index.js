d33b46fe0174117b535595b1b0b174f10aa77fa2
sam@paimon <samuel.vidovich@gmail.com>
2023-10-09 19:55:46 -0400

    New book

--- assets/books/data/index.js
+++ assets/books/data/index.js
@@ -1,7 +1,9 @@
 import { CrvenkapaBook } from "./crvenkapa.js";
 import { OranjeMarkaKraljevicaBook } from "./oranjeMarkaKraljevica.js";
+import { CovijekIZmijaBook } from "./covijekizmija.js";
 
 export const BOOKS = [
   // CrvenkapaBook
   OranjeMarkaKraljevicaBook,
+  CovijekIZmijaBook,
 ];
