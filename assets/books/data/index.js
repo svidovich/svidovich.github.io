a7202b1779e1803a1ba43be9d1a8a56f17a12966
svidovich <samuel.vidovich@gmail.com>
2023-10-22 19:06:20 -0400

    Poems from new Kordun book!

--- assets/books/data/index.js
+++ assets/books/data/index.js
@@ -3,10 +3,20 @@ import { OranjeMarkaKraljevicaBook } from "./oranjeMarkaKraljevica.js";
 import { CovijekIZmijaBook } from "./covijekizmija.js";
 import { CiganinISvabaBook } from "./ciganinisvaba.js";
 import { DobraHvalaBook } from "./dobrahvala.js";
+
+import {
+  SaPlanineVjetarBook,
+  VraniSeKonjiBook,
+  NaGraniciVatraBook,
+} from "./narodnePjesmeKorduna.js";
+
 export const BOOKS = [
   // CrvenkapaBook
   CiganinISvabaBook,
   CovijekIZmijaBook,
   DobraHvalaBook,
+  NaGraniciVatraBook,
   OranjeMarkaKraljevicaBook,
+  SaPlanineVjetarBook,
+  VraniSeKonjiBook,
 ];
---
+++ assets/books/data/narodnePjesmeKorduna.js
@@ -0,0 +1,153 @@
+import { bookFromArray } from "./book.js";
+
+// I'm starting this file with the intention of
+// keeping a bunch of the stories from Kordun
+// Peoples' Songs in it.
+// We will see how that goes.
+// They're in order of the book, God help me.
+
+// #1
+export const SaPlanineVjetarBook = bookFromArray(
+  "Sa Planine Vjetar Duva",
+  "From the Mountains the Wind Blows",
+  "Kordun",
+  "What does Krajina feel like on the heart?",
+  [
+    {
+      latin: "Sa planine hladan vjetar duva",
+      english: "From the mountains a cold wind blows",
+    },
+    {
+      latin: "Tu graničar Austriju čuva;",
+      english: "The border guard of Austria watches;",
+    },
+    {
+      latin: "Oj Krajino, sam te bog ubio!",
+      english: "Oh Krajina, God himself killed you!",
+    },
+    {
+      latin: "Nit mladova, nit se naljubio.",
+      english: "Neither youth nor love.",
+    },
+    {
+      latin: "Dugo dani, a tain mi mali.",
+      english: "Long days and my little secrets.",
+    },
+    {
+      latin: "Teško onom kog država hrani!",
+      english: "It's hard for one whom the state feeds!",
+    },
+    {
+      latin: "Oj Krajino, krvava 'aljino,",
+      english: "Oh Krajina, bloody dress,",
+    },
+    {
+      latin: "Nikad mirna danka za počinka!",
+      english: "Never a quiet day for rest!",
+    },
+    {
+      latin: "S' krvi ruča, a s' krvi večera,",
+      english: "With bloody hands and with bloody dinners,",
+    },
+    {
+      latin: "Svak' krvave švače zalogaje!",
+      english: "Everyone chews bloody morsels!",
+    },
+  ]
+);
+
+// #2
+export const VraniSeKonjiBook = bookFromArray(
+  "Vrani se Konji Igraju",
+  "Black Horses Romped",
+  "Kordun",
+  "Watch the horses play. See the people of the land!",
+  [
+    {
+      latin: "Vrani se konji igraju,",
+      english: "Black horses romped,",
+    },
+    {
+      latin: "Pod sobom zemlju kopaju,",
+      english: "Beneath them the earth they dig,",
+    },
+    {
+      latin: "Zlaćane uzde kidaju,",
+      english: "They tear apart golden reins,",
+    },
+    {
+      latin: "Duga se putu nadaju.",
+      english: "They hope for long journeys.",
+    },
+    {
+      latin: "U kolu djeve igraju,",
+      english: "In a circle the maidens dance,",
+    },
+    {
+      latin: "Valjda se svadbi nadaju!",
+      english: "They probably hope for weddings!",
+    },
+    {
+      latin: "Poljanom momci vježbaju,",
+      english: "In the field the dudes train,",
+    },
+    {
+      latin: "Oni se ratu nadaju.",
+      english: "They hope for war.",
+    },
+    {
+      latin: "Kroz selo mobe pjevaju,",
+      english: "Through the village the masses sing,",
+    },
+    {
+      latin: "Dobroj se žetvi nadaju.",
+      english: "They hope for good harvests.",
+    },
+    {
+      latin: "Stare se babe svađaju,",
+      english: "Old babas argue amongst themselves,",
+    },
+    {
+      latin: "One se smrti nadaju!",
+      english: "They hope for death!",
+    },
+  ]
+);
+
+// #3
+export const NaGraniciVatraBook = bookFromArray(
+  "Na Granici Vatra Gori",
+  "On the Borders the Forests Burn",
+  "Kordun",
+  "A day in the life of a border guard! Will the janissaries make it out alive?",
+  [
+    {
+      latin: "Na granici vatra gori,",
+      english: "On the borders the forests burn,",
+    },
+    {
+      latin: "A prangije glas se ori:",
+      english: "And a raspy voice sings:",
+    },
+    {
+      latin: "Požurite, graničari,",
+      english: "Hurry, border guards,",
+    },
+    {
+      latin: "Krenuli su janjičari!",
+      english: "The janissaries have set off!",
+    },
+    {
+      latin: "Po granica vatra sjeva,",
+      english: "By the border the fire burns,",
+    },
+    {
+      latin: "Junačka se krv proljeva!",
+      english: "Heroes spill their blood!",
+    },
+    {
+      latin: "Umaptiće janjičari kako biju graničari!",
+      english: "The janissaries will remember how the border guards are!",
+    },
+  ]
+);
