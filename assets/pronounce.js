d007612d8b0a522814fa40649c3a4e69afdf54ce
svidovich <samuel.vidovich@gmail.com>
2024-12-23 23:43:58 -0500

    Add keyboard controls. Beef clickables. More tutorial!

--- assets/pronounce.js
+++ assets/pronounce.js
@@ -57,6 +57,7 @@ const stageLetterDatum = (letterDatum, index) => {
   const speakerImgBody = document.createElement("img");
   speakerImgBody.src = "../media/speaker.png";
   speakerImgBody.style.cursor = "pointer";
+  speakerImgBody.id = "playsoundbutton";
   addPlaySound(speakerImgBody, soundKey);
 
   // These are for sample navigation...
@@ -79,6 +80,7 @@ const stageLetterDatum = (letterDatum, index) => {
     rightArrow.style.border = "2px dotted green";
     rightArrow.style.borderRadius = "5px";
     rightArrow.title = "Click for another example.";
+    rightArrow.id = "clickablesamplenavigator";
     rightArrow.addEventListener("click", (event_) => {
       stageLetterDatum(
         letterDatum,
@@ -107,29 +109,98 @@ const stageLetterDatum = (letterDatum, index) => {
 };
 
 // Add a box for containing a letter that we can click on.
-const mkLetterBox = (letterDatum) => {
+const mkLetterBox = (letterDatum, index) => {
   // Making them spans makes them inline by default, which we would like
   const letterBox = document.createElement("span");
+  letterBox.tabIndex = index;
   // Add the latin version to the box. Maybe later we can support the cyrillic.
   letterBox.textContent = letterDatum.latin;
   letterBox.className = "letterbox";
-
   letterBox.addEventListener("click", (event_) => {
     stageLetterDatum(letterDatum);
   });
   return letterBox;
 };
 
+// Dump all of the tab indexes from all page elements.
+// Sorry everybody else! This hopefully makes tab navigation
+// somewhat less of a pain.
+document.querySelectorAll("[tabindex]").forEach((element) => {
+  element.removeAttribute("tabindex");
+});
+
+// A box where we can dump our created letterboxes.
+// We'll use this for tab controlling.
+const letterBoxRegistry = new Object();
+
+let lastIndex = 0;
 // Put the first half of the letters in the header,
-firstHalf.forEach((letterDatum) => {
-  const letterBox = mkLetterBox(letterDatum);
+for (const [index, letterDatum] of firstHalf.entries()) {
+  const letterBox = mkLetterBox(letterDatum, index);
+  letterBoxRegistry[index] = letterBox;
   headerDiv.appendChild(letterBox);
-});
+  lastIndex = index;
+}
 
+let maxIndex = 0;
 // Put the second half of the letters in the footer.
-secondHalf.forEach((letterDatum) => {
-  const letterBox = mkLetterBox(letterDatum);
+for (const [index, letterDatum] of secondHalf.entries()) {
+  // For the index, start where we left off. Careful on that off-by-one.
+  const thisIndex = index + lastIndex + 1;
+  const letterBox = mkLetterBox(letterDatum, thisIndex);
+  letterBoxRegistry[thisIndex] = letterBox;
   footerDiv.appendChild(letterBox);
+  maxIndex = thisIndex;
+}
+
+// State of which index is selected.
+let currentSelection = 0;
+
+const nextLetter = (currentIndex) => {
+  // Stage the next letter, based on global state.
+  if (currentIndex === SAMPLES.length - 1) {
+    currentSelection = 0;
+  } else {
+    currentSelection = currentIndex + 1;
+  }
+  stageLetterDatum(SAMPLES[currentSelection]);
+};
+
+const prevLetter = (currentIndex) => {
+  // Stage the previous letter, based on global state.
+  if (currentIndex === 0) {
+    currentSelection = SAMPLES.length - 1;
+  } else {
+    currentSelection = currentIndex - 1;
+  }
+  stageLetterDatum(SAMPLES[currentSelection]);
+};
+
+// A block of keyboard controls!
+document.addEventListener("keydown", (event_) => {
+  if (event_.key == "Tab") {
+    // Make sure that the browser doesn't act a fool, here. We
+    // don't want it to do typical tab navigation.
+    event_.preventDefault();
+    if (event_.shiftKey === true) {
+      // Press shift + tab to go to the previous letter.
+      prevLetter(currentSelection);
+    } else {
+      // Press tab to go to the next letter.
+      nextLetter(currentSelection);
+    }
+    // Focus the letterbox so usr knows what they're looking at.
+    letterBoxRegistry[currentSelection].focus();
+  } else if (event_.code == "Space") {
+    // Press space to play sounds.
+    document.getElementById("playsoundbutton").click();
+  } else if (event_.key == "Enter") {
+    // Press enter to cycle samples for the current letter.
+    document.getElementById("clickablesamplenavigator").click();
+  }
 });
 
-stageLetterDatum(SAMPLES[0]);
+// Set the first letter to be the default selection,
+stageLetterDatum(SAMPLES[currentSelection]);
+// And make sure its letterbox is focused!
+letterBoxRegistry[currentSelection].focus();
