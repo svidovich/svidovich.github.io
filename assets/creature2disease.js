e728a378e1ea67210147578fe41bf8f9516b29ac
svidovich <samuel.vidovich@gmail.com>
2025-01-22 22:05:43 -0500

    Init creature2disease

---
+++ assets/creature2disease.js
@@ -0,0 +1,117 @@
+const CREATURE_2_DISEASE = [
+  {
+    creature: "Buba",
+    english: "Bug",
+    diseases: ["Gangrene", "Carcinoma", "Apoplexy"],
+  },
+  {
+    creature: "Bubnjaca",
+    english: "Bug",
+    condition: ["Boils"],
+  },
+  {
+    creature: "Bubuljica",
+    english: "Little Bug",
+    condition: ["Pimple"],
+  },
+  {
+    creature: "Zla Buba",
+    english: "Evil Bug",
+    condition: ["Cuticle Inflammation", "Gumboil"],
+  },
+  {
+    creature: "Buba u Glavi",
+    english: "Bug in the Head",
+    condition: ["Brain Fever", "Jaundice"],
+  },
+  {
+    creature: "Badlje",
+    english: "Beetle",
+    condition: ["Sty", "Trichiasis"],
+  },
+  {
+    creature: "Crv",
+    english: "Worm",
+    condition: ["Scalp Disease"],
+  },
+  {
+    creature: "Kušćer",
+    english: "Lizard",
+    condition: ["Swelling", "Sprain"],
+  },
+  {
+    creature: "Metiljka",
+    english: "Moth",
+    condition: ["Cataract"],
+  },
+  {
+    creature: "Mlamosi",
+    english: "Centipede",
+    condition: ["Heat Stroke"],
+  },
+  {
+    creature: "Magarac",
+    english: "Donkey",
+    condition: ["Whooping Cough"],
+  },
+  {
+    creature: "Mrava",
+    english: "Ants",
+    condition: ["Chills from Fever"],
+  },
+  {
+    creature: "Mačka",
+    english: "Cat",
+    condition: ["Headcold"],
+  },
+  {
+    creature: "Pauk",
+    english: "Spider",
+    condition: ["Syphilis"],
+  },
+  {
+    creature: "Nerast",
+    english: "Juvenile Boar",
+    condition: ["Splenitis"],
+  },
+  {
+    creature: "Ospice",
+    english: "Wasps",
+    condition: ["Pox"],
+  },
+  {
+    creature: "Rak",
+    english: "Crab",
+    condition: ["Cancer"],
+  },
+  {
+    creature: "Štene",
+    english: "Puppy",
+    condition: ["Festering Wound"],
+  },
+  {
+    creature: "Svinja",
+    english: "Pig",
+    condition: ["Scrofulosis", "Fistula"],
+  },
+  {
+    creature: "Trud",
+    english: "Hornet",
+    condition: ["Ulcer"],
+  },
+  {
+    creature: "Gogolj",
+    english: "Diving Bird",
+    condition: ["Swelling"],
+  },
+  {
+    creature: "Guta",
+    english: "Pigeon",
+    condition: ["Diptheria"],
+  },
+  {
+    creature: "",
+    english: "",
+    condition: [],
+  },
+];
